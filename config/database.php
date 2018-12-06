0e33fe6da43ec41fdcda1cbd1a713b24135504ea
Sebastian <sebas14121999@gmail.com>
2018-12-05 22:11:23 -0500

    P5

--- config/database.php
+++ config/database.php
@@ -97,7 +97,7 @@ return [
 
         'mongodb' => [ 
             'driver'    =>  'mongodb', 
-            'host'      =>  env('DB_HOST','mongodb://sebastian:sebas123@ds149742.mlab.com:49742/integrador'),
+            'host'      =>  env('DB_HOST','sebastian:sebas123@ds149742.mlab.com:49742/integrador'),
             'port'      =>  env('DB_PORT'),
             'database'  =>  env('DB_DATABASE'),
             'username'  =>  env('DB_UERNAME'),
